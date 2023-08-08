c895b70c4021def23df11f5863062788c54c73b6
Bart Bas Elsenaar <advendelie@hotmail.com>
2023-08-08 12:10:06 +0200

    ADD: Links! and more tests

--- resources/views/inspire.blade.php
+++ resources/views/inspire.blade.php
@@ -1,7 +1,7 @@
 <html>
 <link rel="stylesheet" type="text/css" href={{asset('css/app.css')}}>
 <body>
-This will be the next page for the tests <br>
+This will be the next page for the tests now WITH CSS<br>
 
 <a href="/">TAKE ME HOME</a>.
 </body>
