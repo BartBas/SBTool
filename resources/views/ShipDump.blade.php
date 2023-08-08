a5a4d57b04073c001752115e1064debef37e1ff6
Bart Bas Elsenaar <advendelie@hotmail.com>
2023-08-08 16:48:19 +0200

    ADD: Links! and more tests with extra things

--- resources/views/ShipDump.blade.php
+++ resources/views/ShipDump.blade.php
@@ -5,21 +5,6 @@
 </head>
 <body>
 
-<label for="cars">Choose a car:</label>
-<select name="cars" id="cars" form="carform">
-    @foreach($names as $name)
-        <option value={{$name}}>{{$name}}</option>
-
-
-    @endforeach
-    <option value="volvo">Volvo</option>
-    <option value="saab">Saab</option>
-    <option value="opel">Opel</option>
-    <option value="audi">Audi</option>
-</select>
-
-
-
 <table>
     <tr>
     @foreach(array_keys($table[0]) as $keys)
